73a93575806eeebc738933e062322a4906567e63
Akash <akash.babu@travanleo.com>
2023-01-09 03:52:57 +0530

    praxis changes

---
+++ src/main.js
@@ -0,0 +1,18 @@
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".section-2").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+    console.log(oTop);
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
