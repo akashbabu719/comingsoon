7efb993ad923b90c13b9a00bd054f43fc99ae1a3
Akash Babu <akashbabu719@gmail.com>
2023-01-09 20:07:08 +0530

    style fix

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ let navbar = $(".navbar");
 
 $(window).scroll(function () {
   // get the complete hight of window
-  let oTop = $(".section-4").offset().top - window.innerHeight;
+  let oTop = $(".section-2").offset().top - window.innerHeight;
   if ($(window).scrollTop() > oTop) {
     navbar.addClass("sticky");
     console.log(oTop);
