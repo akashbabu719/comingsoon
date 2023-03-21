b29a44546ba80501fb4095e322ee2544b429dafc
Akash <akash.babu@travanleo.com>
2023-03-21 23:58:23 +0530

    Language switcher changes

--- src/main.js
+++ src/main.js
@@ -16,3 +16,28 @@ $(window).scroll(function () {
     navbar.removeClass("sticky");
   }
 });
+
+// JSON data for the languages
+const data = {
+  "en": {
+    "title": "Welcome!",
+    "address": "Facharzt Innere Medizin, Zusatzweiterbildung Intensivmedizin <br> Saarstr.4, 38440 Wolfsburg"
+  },
+  "de": {
+    "title": "Willkommen!",
+    "address": "Saarstraße 4, 38440 Wolfsburg"
+  }
+};
+
+function updateLanguage(language) {
+  document.getElementById("title").innerHTML = data[language].title;
+  document.getElementById("address").innerHTML = data[language].address;
+}
+
+document.getElementById("language-toggle").addEventListener("change", function() {
+  const language = this.checked ? "en" : "de";
+  updateLanguage(language);
+});
+
+// Set the initial language to English
+updateLanguage("de");
