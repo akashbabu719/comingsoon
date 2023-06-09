0f38e51820e553234c10bcaadc8439b684b828bf
Akash <akash.babu@travanleo.com>
2023-06-10 00:16:01 +0530

    Emergency consultation hours

--- src/main.js
+++ src/main.js
@@ -21,17 +21,20 @@ $(window).scroll(function () {
 const data = {
   "en": {
     "title": "Welcome!",
-    "address": "Facharzt Innere Medizin, Facharzt Kardiologie,<br> Zusatzweiterbildung Intensivmedizin <br> Saarstr.4, 38440 Wolfsburg"
+    "address": "Facharzt Innere Medizin, Facharzt Kardiologie,<br> Zusatzweiterbildung Intensivmedizin <br> Saarstr.4, 38440 Wolfsburg",
+    "emergencyConsltaionHours": "<b>Emergency consultation hours </b><br> Mon, Tue, Wed, Thu, Fri - 08:00 - 09:00."
   },
   "de": {
     "title": "Willkommen!",
-    "address": "Facharzt Innere Medizin, Facharzt Kardiologie,<br> Zusatzweiterbildung Intensivmedizin <br> Saarstr.4, 38440 Wolfsburg"
+    "address": "Facharzt Innere Medizin, Facharzt Kardiologie,<br> Zusatzweiterbildung Intensivmedizin <br> Saarstr.4, 38440 Wolfsburg",
+    "emergencyConsltaionHours": "<b>Notfall Sprechzeiten hours </b><br> Mo, Di, Mi, Do, Fr - 08:00 - 09:00 Uhr."
   }
 };
 
 function updateLanguage(language) {
   document.getElementById("title").innerHTML = data[language].title;
   document.getElementById("address").innerHTML = data[language].address;
+  document.getElementById("emergencyConsltaionHours").innerHTML = data[language].emergencyConsltaionHours;
 }
 
 document.getElementById("language-toggle").addEventListener("change", function() {
